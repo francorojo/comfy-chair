f404da19e9f3ddf61b17976360ac7a403ccac057
Nicolas Madeo <nicomad.17@gmail.com>
2024-07-31 02:01:55 -0300

    session reception tests added

--- src/tests/session.test.ts
+++ src/tests/session.test.ts
@@ -34,9 +34,40 @@ describe('test session case use', () => {
 			session.addArticle(regularArticleDummy);
 		}).toThrow(new Error('This session can not recive more articles'));
 	});
+
+	test('Session should start with RECEPTION state', () => {
+		const session = new Session('Test', 1, top3SelectionDummy);
+		expect(SessionState.RECEPTION).toEqual(session.getState());
+	});
+
+	test('Session should be able to receive a RegularArticle in RECEPTION state', () => {
+		const session = new Session('Test', 1, top3SelectionDummy);
+		session.updateState(SessionState.RECEPTION);
+		session.addArticle(regularArticleDummy);
+		expect(1).toEqual(session.getArticles().length);
+	});
+
+	test('Session should not be able to receive a RegularArticle in BIDDING state', () => {
+		const session = new Session('Test', 1, top3SelectionDummy);
 		session.updateState(SessionState.BIDDING);
 		expect(() => {
-			session.addArticle(article);
+			session.addArticle(regularArticleDummy);
+		}).toThrow(new Error('This session can not recive more articles'));
+	});
+
+	test('Session should not be able to receive a RegularArticle in SELECTION state', () => {
+		const session = new Session('Test', 1, top3SelectionDummy);
+		session.updateState(SessionState.SELECTION);
+		expect(() => {
+			session.addArticle(regularArticleDummy);
+		}).toThrow(new Error('This session can not recive more articles'));
+	});
+
+	test('Session should not be able to receive a RegularArticle in ASIGMENTANDREVIEW state', () => {
+		const session = new Session('Test', 1, top3SelectionDummy);
+		session.updateState(SessionState.ASIGMENTANDREVIEW);
+		expect(() => {
+			session.addArticle(regularArticleDummy);
 		}).toThrow(new Error('This session can not recive more articles'));
 	});
 });
