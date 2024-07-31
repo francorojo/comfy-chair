f95973804309b2b137be61124b20b6d021316e17
Franco Bortolazzi Conte <franco.bortolazzi@mercadolibre.com>
2024-07-30 21:20:15 -0300

    Add session, type and selection (#11)
    * Add session, type and selection
    
    * Refactor session
    
    * Remove folder

---
+++ src/app/session.ts
@@ -0,0 +1,72 @@
+import { RegularArticle } from "./article";
+
+export class Session {
+    private theme: string;
+    private state: SessionState;
+    private maxArticlesAccept: number;
+    private selectionForm: Map<SessionType,SessionSelection>;
+    private articles: RegularArticle[];
+
+    public constructor(theme: string, maxArticlesAccept: number, selectionForm: Map<SessionType,SessionSelection>) {
+        this.theme = theme;
+        this.maxArticlesAccept = maxArticlesAccept;
+        this.selectionForm = selectionForm;
+
+        //Init default
+        this.state =  SessionState.RECEPTION;
+        this.articles = [];
+      }
+
+    public getTheme(): string {
+        return this.theme;
+    }
+
+    public getState(): SessionState {
+        return this.state;
+    }
+
+    public getMaxArticlesAccept(): number {
+        return this.maxArticlesAccept;
+    }
+
+    public getSelectionForm(): Map<SessionType,SessionSelection> {
+        return this.selectionForm;
+    }
+
+    public addArticle(article: RegularArticle) {
+        //Validations to add a new article
+        if( this.articles.length == this.maxArticlesAccept )
+            throw new Error('The number of items exceeds the maximum allowed');
+        if ( this.state != SessionState.RECEPTION)
+            throw new Error('This session can not recive more articles');
+        
+        return this.articles.push(article);
+    }
+
+    public getArticles() {
+        return this.articles;
+    }
+
+    public updateState(state: SessionState){
+        return this.state = state;
+    }
+    
+}
+
+export enum SessionSelection {
+    TOP3,
+    MINVALUE
+}
+
+export enum SessionType {
+    REGULAR,
+    POSTER
+}
+
+//Maybe in future this enum can be a state pattern for delegate logic
+export enum SessionState {
+    RECEPTION,
+    BIDDING,
+    ASIGMENTANDREVIEW,
+    SELECTION
+}
---
+++ src/tests/session.test.ts
@@ -0,0 +1,48 @@
+import { Session, SessionType, SessionSelection, SessionState } from "@app/session";
+import { RegularArticle } from "@app/article"
+
+describe('test session case use', () => {
+  
+
+    test('Create a new session correctly', () => {
+        let map = new Map();
+        map.set(SessionType.POSTER, SessionSelection.TOP3); 
+        const abstract =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque orci metus, dignissim";
+        const title = "Sample Article Title";
+        const authors = ["Author 1", "Author 2"];
+        const article = new RegularArticle(abstract, title, authors);
+        const session = new Session("Test", 2, map);
+        session.addArticle(article);
+        expect("Test").toEqual(session.getTheme());
+        expect(SessionState.RECEPTION).toEqual(session.getState());
+        expect(2).toEqual(session.getMaxArticlesAccept());
+        expect(1).toEqual(session.getArticles().length);
+    });
+
+    test('Create a new session with more articles allowed', () => {
+        let map = new Map();
+        map.set(SessionType.POSTER, SessionSelection.TOP3); 
+        const abstract =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque orci metus, dignissim";
+        const title = "Sample Article Title";
+        const authors = ["Author 1", "Author 2"];
+        const article = new RegularArticle(abstract, title, authors);
+        const session = new Session("Test", 0, map);
+        expect( () => { session.addArticle(article)} ).toThrow(new Error("The number of items exceeds the maximum allowed"));
+    });
+
+    test('Create a new session, update state and try add new article', () => {
+        let map = new Map();
+        map.set(SessionType.POSTER, SessionSelection.TOP3); 
+        const abstract =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque orci metus, dignissim";
+        const title = "Sample Article Title";
+        const authors = ["Author 1", "Author 2"];
+        const article = new RegularArticle(abstract, title, authors);
+        const session = new Session("Test", 1, map);
+        session.updateState(SessionState.BIDDING);
+        expect( () => { session.addArticle(article)} ).toThrow(new Error("This session can not recive more articles"));
+    });
+
+});
