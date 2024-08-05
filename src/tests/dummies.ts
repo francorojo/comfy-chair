ff70f3a1c0caea739d51190b26e38aba889ad91c
Maddness <49067443+NicoMadd@users.noreply.github.com>
2024-08-05 19:22:37 -0300

    Feature/reception-phase (#16)
    * prettier config added
    
    * prettier added to project
    
    * fix prettier config
    
    * prettier script added to project
    
    * format file projects
    
    * prettier github action added
    
    * default node version added
    
    * node version adjusted based on nvmrc
    
    * fix event trigger of prettier action
    
    * tests path added to tsconfig
    
    * dummies refactor extrated
    
    * fix jest config
    
    * session tests fix
    
    * session accepts articles
    
    * article and selection dummies added
    
    * session tests refactor
    
    * session reception tests added
    
    * poster dummy added
    
    * poster session tests added
    
    * deadline added to session
    
    * fix prettier config
    
    * fix merge

--- src/tests/dummies.ts
+++ src/tests/dummies.ts
@@ -1,3 +1,5 @@
+import {RegularArticle, Poster} from '@app/article'
+import {SessionType, SessionSelection} from '@app/session'
 import {Rol, User} from '@app/user'
 
 // USER DUMMIES
@@ -32,4 +34,38 @@ export const dummyAuthor3 = new User(
 	'bob@utn.frba.edu.ar',
 	'test-2024-UTN',
 	Rol.AUTHOR
+)
+
+// SESSION SELECTION DUMMIES
+
+export const top3SelectionDummy = new Map<SessionType, SessionSelection>([
+	[SessionType.POSTER, SessionSelection.TOP3]
+])
+
+// DUMMY ARTICLES
+
+const abstract =
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque orci metus, dignissim'
+const title = 'Sample Article Title'
+const authors = [dummyAuthor1, dummyAuthor2]
+const notificationAuthor = dummyAuthor1
+const fileURL = 'https://example.com/sample-article.pdf'
+
+export const regularArticleDummy = new RegularArticle(
+	abstract,
+	title,
+	authors,
+	notificationAuthor,
+	fileURL
+)
+
+const posterTitle = 'Sample Poster Title'
+const sourceURL = 'https://example.com/sample-poster.pdf'
+
+export const posterArticleDummy = new Poster(
+	posterTitle,
+	authors,
+	notificationAuthor,
+	fileURL,
+	sourceURL
 )
