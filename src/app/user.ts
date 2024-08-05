0a6a5c097acc81c87e2915f592af1d4f46e1ed94
Maddness <49067443+NicoMadd@users.noreply.github.com>
2024-08-04 22:50:12 -0300

    Feature/prettier (#14)
    * prettier config added
    
    * prettier added to project
    
    * fix prettier config
    
    * prettier script added to project
    
    * format file projects
    
    * prettier github action added
    
    * default node version added
    
    * node version adjusted based on nvmrc
    
    * fix event trigger of prettier action
    
    * fix prettier config
    
    * fix prettier config

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,49 +1,56 @@
 export class User {
-    private name: string;
-    private membership: string;
-    private email: string;
-    private password: string;
-    private rol: Rol;
+	private name: string
+	private membership: string
+	private email: string
+	private password: string
+	private rol: Rol
 
-    public constructor(name: string, membership: string, email: string, password: string, rol: Rol) {
-        if( password.length < 8)
-            throw new Error('This value is required and must to be greater 8 caracters');
-        
-        this.name = name;
-        this.membership =  membership;
-        this.email = email;
-        this.password = password;
-        this.rol = rol;
-      }
+	public constructor(
+		name: string,
+		membership: string,
+		email: string,
+		password: string,
+		rol: Rol
+	) {
+		if (password.length < 8)
+			throw new Error(
+				'This value is required and must to be greater 8 caracters'
+			)
 
-    public getName(): string {
-        return this.name;
-    }
+		this.name = name
+		this.membership = membership
+		this.email = email
+		this.password = password
+		this.rol = rol
+	}
 
-    public getMembership(): string {
-        return this.membership;
-    }
+	public getName(): string {
+		return this.name
+	}
 
-    public getEmail(): string {
-        return this.email;
-    }
+	public getMembership(): string {
+		return this.membership
+	}
 
-    public getPassword(): string {
-        return this.password;
-    }
+	public getEmail(): string {
+		return this.email
+	}
 
-    public getRol(): Rol {
-        return this.rol;
-    }
+	public getPassword(): string {
+		return this.password
+	}
 
-    public serRol(rol: Rol): Rol {
-        return this.rol=rol;
-    }
-    
+	public getRol(): Rol {
+		return this.rol
+	}
+
+	public serRol(rol: Rol): Rol {
+		return (this.rol = rol)
+	}
 }
 
 export enum Rol {
-    ORGANIZER,
-    REVIEWER,
-    AUTHOR
+	ORGANIZER,
+	REVIEWER,
+	AUTHOR
 }
--- src/tests/dummies.ts
+++ src/tests/dummies.ts
@@ -1,36 +1,35 @@
-import { Rol, User } from "@app/user"
+import {Rol, User} from '@app/user'
 
 // USER DUMMIES
 
 export const dummyOrganizer = new User(
-    "Jane",
-    "UTN-FRBA",
-    "jane@utn.frba.edu.ar",
-    "test-2024-UTN",
-    Rol.ORGANIZER
+	'Jane',
+	'UTN-FRBA',
+	'jane@utn.frba.edu.ar',
+	'test-2024-UTN',
+	Rol.ORGANIZER
 )
 
 export const dummyAuthor1 = new User(
-    "Jane",
-    "UTN-FRBA",
-    "jane@utn.frba.edu.ar",
-    "test-2024-UTN",
-    Rol.AUTHOR
+	'Jane',
+	'UTN-FRBA',
+	'jane@utn.frba.edu.ar',
+	'test-2024-UTN',
+	Rol.AUTHOR
 )
 
 export const dummyAuthor2 = new User(
-    "John",
-    "UTN-FRBA",
-    "john@utn.frba.edu.ar",
-    "test-2024-UTN",
-    Rol.AUTHOR
+	'John',
+	'UTN-FRBA',
+	'john@utn.frba.edu.ar',
+	'test-2024-UTN',
+	Rol.AUTHOR
 )
 
 export const dummyAuthor3 = new User(
-    "Bob",
-    "UTN-FRBA",
-    "bob@utn.frba.edu.ar",
-    "test-2024-UTN",
-    Rol.AUTHOR
+	'Bob',
+	'UTN-FRBA',
+	'bob@utn.frba.edu.ar',
+	'test-2024-UTN',
+	Rol.AUTHOR
 )
-
